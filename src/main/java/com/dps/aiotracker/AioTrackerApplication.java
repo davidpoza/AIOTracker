6c9579c72a9eb060bb7ae9103e1546225f86d7a5
David Poza Suárez <pozasuarez@gmail.com>
2024-07-19 21:16:07 +0200

    first commit

---
+++ src/main/java/com/dps/aiotracker/AioTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dps.aiotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AioTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AioTrackerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/controllers/ClientsRestController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController(value = "/clients")
+@Tag(name = "Clients")
+@Api(value = "API to search Student from a Student Repository by different search parameters",
+description = "This API provides the capability to search Student from a Student Repository", produces = "application/json")
+public class ClientsRestController {
+
+    @Operation(summary = "This method is used to get the clients.")
+    @GetMapping
+    public List<String> getClients() {
+        return Arrays.asList("First Client", "Second Client");
+    }
+}
