8408ae320122e51e6becbd9c353c0d07a65ab18e
David Poza Suárez <pozasuarez@gmail.com>
2024-07-25 11:03:53 +0200

    customer dto

--- src/main/java/com/dps/aiotracker/mapper/CustomerMapper.java
+++ src/main/java/com/dps/aiotracker/mapper/CustomerMapper.java
@@ -1,16 +1,22 @@
 package com.dps.aiotracker.mapper;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import com.dps.aiotracker.domain.Customer;
 import com.dps.aiotracker.dto.CustomerDTO;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface CustomerMapper {
 	CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
 
 	CustomerDTO toDto(Customer customer);
 
 	Customer toEntity(CustomerDTO customerDTO);
+
+	List<CustomerDTO> toDto(List<Customer> customers);
+
+	List<Customer> toEntity(List<CustomerDTO> customerDTOs);
 }
--- src/main/java/com/dps/aiotracker/service/CustomerService.java
+++ src/main/java/com/dps/aiotracker/service/CustomerService.java
@@ -1,7 +1,6 @@
 package com.dps.aiotracker.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,7 +20,8 @@ public class CustomerService {
 	private CustomerMapper customerMapper;
 
 	public List<CustomerDTO> getAllCustomers() {
-		return customerRepository.findAll().stream().map(customerMapper::toDto).collect(Collectors.toList());
+		List<Customer> resultado = customerRepository.findAll();
+		return customerMapper.toDto(resultado);
 	}
 
 	public CustomerDTO getCustomerById(Long id) {
